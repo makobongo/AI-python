dc176a8ebb18f9bfc3c3bc60c4b16d4c33c83759
steve obongo <steveobongo0@gmail.com>
2025-05-24 22:05:54 +0300

    fix: adjusted how to import OpenAI key

--- patterns/workflows/basic.py
+++ patterns/workflows/basic.py
@@ -2,7 +2,9 @@ import os
 
 from openai import OpenAI
 
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(
+    api_key = os.getenv("OPENAI_API_KEY")
+)
 
 completion = client.chat.completions.create(
     model="chatgpt-4o-latest",
